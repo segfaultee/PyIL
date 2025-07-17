0c8e80020d6ebc10fa19ec5a921ed6cfe2c0958c
segfaultee <58683525+segfaultee@users.noreply.github.com>
2025-07-17 12:05:50 +0400

    feat(runtime): add dll entry

---
+++ PyIL/runtime/console.hh
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+#include <windows.h>
+
+namespace pyil::console
+{
+    inline HANDLE out_handle = nullptr;
+
+    inline void init()
+    {
+        AllocConsole();
+
+        out_handle = CreateFileA(
+            "CONOUT$", GENERIC_WRITE, FILE_SHARE_WRITE, nullptr,
+            OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, nullptr
+        );
+
+        FILE* f;
+        freopen_s(&f, "CONOUT$", "w", stdout);
+
+        std::ios::sync_with_stdio();
+    }
+
+    inline void deinit()
+    {
+        if (out_handle != INVALID_HANDLE_VALUE)
+        {
+            CloseHandle(out_handle);
+            out_handle = INVALID_HANDLE_VALUE;
+        }
+
+        FreeConsole();
+    }
+
+    inline void write(const std::string& msg)
+    {
+        DWORD w;
+        WriteConsoleA(out_handle, msg.c_str(), (DWORD)msg.size(), &w, nullptr);
+    }
+}
---
+++ PyIL/runtime/entry.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <format>
+
+#include <windows.h>
+
+#include "il2cpp/il2cpp.hh"
+
+#include "console.hh"
+
+void entry()
+{
+    pyil::console::init();
+
+    if (!il2cpp::init())
+        return;
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD reason, LPVOID)
+{
+    switch (reason)
+    {
+        case DLL_PROCESS_ATTACH:
+            DisableThreadLibraryCalls(hModule);
+            CreateThread(nullptr, 0, (LPTHREAD_START_ROUTINE)entry, nullptr, 0, nullptr);
+            break;
+    }
+    return TRUE;
+}
