3746032c3316e98d50edcbf852feb80312ff7204
segfaultee <58683525+segfaultee@users.noreply.github.com>
2025-07-21 08:54:59 +0400

    feat(sdk): add stub files for RT bindings

---
+++ PyIL/sdk/src/pyil/il2cpp/__init__.pyi
@@ -0,0 +1,7 @@
+def select_assembly(name: str) -> None:
+    """
+    Selects the assembly to be used by PyIL.
+
+    Args:
+        name (str): The name of the assembly to select (e.g "Assembly-CSharp").
+    """
